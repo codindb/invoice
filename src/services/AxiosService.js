6b973f33d3fde524860775453edc3dfc1f684902
codindb <codindb.dev@gmail.com>
2021-09-27 15:10:06 +0200

    EDIT: env variable

--- src/services/AxiosService.js
+++ src/services/AxiosService.js
@@ -1,16 +1,15 @@
 import axios from "axios";
 
-const APIKEY = import.meta.env.VITE_APIKEY
+const AIRTABLEKEY = import.meta.env.VITE_AIRTABLEKEY
 
 // axios instance definition
 const instance = axios.create({
    baseURL: "https://api.airtable.com/v0/appN5GH2CalsuxEiS/Demande%20d'inscription/",
-   headers: { 'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + APIKEY}
+   headers: { 'Content-Type': 'application/json; charset=utf-8', 'Authorization': 'Bearer ' + AIRTABLEKEY}
 })
 
 // Get record by id
 export async function getRecord (id) {
    const res = await instance.get('/' + id)
-   console.log(`API KEY: ${APIKEY}`);
    return res.data
 }
